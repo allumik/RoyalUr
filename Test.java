3cda07b7034c5f3c0b8a8509f68caccce4df9121
Allumik <33635216+Allumik@users.noreply.github.com>
2018-04-02 17:11:16 +0300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+	
+	public static void main(String[] args) {
+		Täring täring = new Täring();
+		System.out.println(täring.getClass());
+		System.out.println(täring.getVise());
+		System.out.println(täring.getVisetäring());
+	}
+}
