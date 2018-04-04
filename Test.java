73f457af09dfab732c1f23abcc7f45db94a08df4
screaminglordbyron <37482300+screaminglordbyron@users.noreply.github.com>
2018-04-04 06:53:48 +0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,11 @@
 public class Test {
 	
 	public static void main(String[] args) {
+		Kasutajaliides kasutajaliides = new Kasutajaliides();
+        	kasutajaliides.kysimepoolt();
+        	int kasutajapool = kasutajaliides.getValitudpool(); /* küsime kasutajalt soovitud poolt (valge 1,must 2) */
+        	/* kasutajaliides.nupuvalimine();
+        	int nupuvalik = kasutajaliides.getNupuvalik();uue nupu voi olemasoleva valimine*/
 		Täring täring = new Täring();
 		System.out.println(täring.getViseTäringud());
 		
